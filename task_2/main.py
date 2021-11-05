d51b8a57cd6f01c2f519133b66d0e908602df524
ppa <lab_ppa@mail.ru>
2021-11-05 16:52:58 +0300

    lesson2 all task complit

--- task_2/main.py
+++ task_2/main.py
@@ -53,27 +53,26 @@
 а потом сохранять все в файл
 """
 
-
 import json
 
 
+def get_data():
+    data = list()
+    data.append(['принтер', '10', '6700', 'Ivanov I.I.', '24.09.2017'])
+    data.append(['scaner', '20', '10000', 'Petrov P.P.', '11.01.2018'])
+    return data
+
+
 def write_order_to_json(item, quantity, price, buyer, date):
-    """Запись в json"""
-
-    with open('orders_1.json', 'r', encoding='utf-8') as f_out:
-        data = json.load(f_out)
-
-    with open('orders_2.json', 'w', encoding='utf-8') as f_in:
-        orders_list = data['orders']
-        order_info = {'item': item, 'quantity': quantity,
-                      'price': price, 'buyer': buyer, 'date': date}
-        orders_list.append(order_info)
-        json.dump(data, f_in, indent=4)
+    data = {}
+    with open('orders_1.json', encoding='utf-8') as infile:
+        data = json.load(infile)
+    orders = data['orders']
+    orders.append({"item": item, "quantity": quantity, "price": price, "buyer": buyer, "date": date})
+    with open("orders_1.json", "w", encoding='utf-8') as orders_file:
+        json.dump(data, orders_file, indent=4, ensure_ascii=False)
 
 
-# Вот здесь важный момент. С латиницей все хорошо. Но стоит указать строку с кириллицей
-# и в json-файле получим
-# "item": "\u043f\u0440\u0438\u043d\u0442\u0435\u0440",
-write_order_to_json('printer', '10', '6700', 'Ivanov I.I.', '24.09.2017')
-write_order_to_json('scaner', '20', '10000', 'Petrov P.P.', '11.01.2018')
-write_order_to_json('computer', '5', '40000', 'Sidorov S.S.', '2.05.2019')
+for i in get_data():
+    if len(i) == 5:
+        write_order_to_json(i[0], i[1], i[2], i[3], i[4])
--- task_3/main.py
+++ task_3/main.py
@@ -22,17 +22,21 @@ ASCII(например, €);
 import yaml
 
 DATA_IN = {'items': ['computer', 'printer', 'keyboard', 'mouse'],
+           'items_ptice': {'computer': '200€-1000€',
+                           'keyboard': '5€-50€',
+                           'mouse': '4€-7€',
+                           'printer': '100€-300€'},
            'items_quantity': 4,
-           'items_ptice': {'computer': '200\u20ac-1000\u20ac',
-                           'printer': '100\u20ac-300\u20ac',
-                           'keyboard': '5\u20ac-50\u20ac',
-                           'mouse': '4\u20ac-7\u20ac'}
            }
 
-with open('file_2.yaml', 'w', encoding='utf-8') as f_in:
+with open('file_r.yaml', 'w', encoding='utf-8') as f_in:
     yaml.dump(DATA_IN, f_in, default_flow_style=False, allow_unicode=True, sort_keys=False)
 
-with open("file_2.yaml", 'r', encoding='utf-8') as f_out:
+with open("file_r.yaml", 'r', encoding='utf-8') as f_out:
     DATA_OUT = yaml.load(f_out, Loader=yaml.SafeLoader)
 
-print(DATA_IN == DATA_OUT)
+with open("file.yaml", 'r', encoding='utf-8') as f_out:
+    DATA_ISHOD = yaml.load(f_out, Loader=yaml.SafeLoader)
+
+print('проверка на совпадение =', DATA_IN == DATA_OUT, ' сгенерированного и сохраненного')
+print('проверка на совпадение =', DATA_ISHOD == DATA_OUT, ' исходного и сохраненного')
